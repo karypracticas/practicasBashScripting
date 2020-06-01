0ae79ce9e905c51aeddbfcc271dad739f4bcd7db
Kary Rguez <practicasgithub@gmail.com>
2020-06-01 17:46:46 -0500

    Practicas de bash scripting

---
+++ 1_utilityPostgres.sh
@@ -0,0 +1,4 @@
+# !/bin/bash
+# Programa para realizar algunas operaciones utilitarias de Posgres
+
+echo "Bienvenido al curso de Programación Bash"
---
+++ 3_tipoOperadores.sh
@@ -0,0 +1,13 @@
+# ! /bin/bash
+# Programa para revisar los tipos de operadores
+# Autor: Kary Rguez
+
+numA=10
+numB=4
+
+echo "Operadores aritméticos"
+echo "Numeros: A=$numA y B=$numB"
+echo "Sumar A + B = " $((numA + numB))
+
+echo "Restar A - B = " $((numA - numB))
+
