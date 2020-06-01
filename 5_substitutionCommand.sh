fd6cfedfb6d4bf5a7008dad42f82f742c8bb6bfc
Kary Rguez <practicasgithub@gmail.com>
2020-06-01 18:12:47 -0500

    Practica de comando de sustitucion y practica de reto1

---
+++ 5_substitutionCommand.sh
@@ -0,0 +1,9 @@
+# ! /bin/bash
+# Programa para revisar como ejecutar comandos dentro de un programa y almacenar en una variable para su posterior utilización
+# Autor: Kary Rguez
+
+infoKernel=$(uname -a)
+
+echo "Información del Kernel:$infoKernel"
+
+
---
+++ utilityHost.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+# Programa para declarar dos variables e imprimir sus valores
+
+option=1
+result="Mi programa bash"
+
+echo "La opción es: $option y el resultado es: $result"
