e98c90b68f0b3c2024028f95e2cc522f9dc0fc48
Kary Rguez <practicasgithub@gmail.com>
2020-06-12 21:21:30 -0500

    Break y Continue, el ejemplo tiene un issue al ejecutarse

---
+++ 19_breakContinue.sh
@@ -0,0 +1,19 @@
+# ! /bin/bash
+# Programa para ejemplificar el uso de break y continue
+# Autor: Kary Rodriguez
+
+echo "Sentencias break y continue"
+for fil in $(ls)
+do
+    for nombre in {1..4}
+    do
+        if [ $fil = "10_download.sh"]; then
+            break;
+        elif [[ $fil == 4* ]];
+            continue;
+        else
+            echo "Nombre archivo:$fil _ $nombre"
+        fi
+    done
+done
+
