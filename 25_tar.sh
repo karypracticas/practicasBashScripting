f7908b576789733453fe800459e98b925d93c79f
Kary Rguez <practicasgithub@gmail.com>
2020-06-18 14:49:09 -0500

    Empaquetar en un archivo tar

---
+++ 25_tar.sh
@@ -0,0 +1,8 @@
+# ! /bin/bash
+# Programa para ejemplificar el empaquetamiento con el comando tar
+# Autor: Kary Rodriguez
+
+echo "Empaquetar todos los scripts de la carpeta shellCourse"
+tar -cvf shellCourse.tar *.sh
+
+
