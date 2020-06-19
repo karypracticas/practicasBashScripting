5ae72edfbaac06cac17b2ee881ba99576966e5ac
Kary Rguez <practicasgithub@gmail.com>
2020-06-19 13:19:29 -0500

    transferir por la red utilizando el comando rsync, utilizando las opciones de empaquetamiento para optimizar la velocidad de transferencia

---
+++ 29_pachageSSH.sh
@@ -0,0 +1,15 @@
+# ! /bin/bash
+# Programa para ejemplificar la forma de como transferir por la red utilizando el comando rsync, utilizando las opciones de empaquetamiento para optimizar la velocidad de transferencia
+# Autor: Kary Rodriguez
+
+echo "Empaquetar todos los scripts de la carpeta shellCourse y transferirlos por la red a otro equipo utilizando el comando rsync"
+
+host=""
+usuario=""
+
+read -p "Ingresar el host:" host
+read -p "Ingresar el usuario:" usuario
+echo "\nEn este momento se procederá a empaquetar la carpeta y transferir según los datos ingresados"
+
+rsync -avz $(pwd) $usuario@$host:/Users/martrosfe/Downloads/Platzi
+
